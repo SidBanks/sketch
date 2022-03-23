7ae64eb3afd92ed4545df245d1855cfac5eff2e5
Sid <sidbanks@gmail.com>
2022-03-23 01:06:12 -0500

    Added input prompt function.

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ let input;
 
 // function that takes input from gridSize button and squares it then
 // sets it to the gridBox variable.
-function gridConversion (input) {
+function gridConversion(input) {
      gridSize = input * input;
      return gridSize;
 }
 
 // function using for loop and gridBox variable to create divs
+function gridLayout(gridSize) {
+     
+}
+
+// function for using button ts resize grid.
+input = prompt("Please enter a number between 0 -100 to resize the sketch pad.");
+if (isNaN(input)) {
+     alert('Please Enter a number');
+} else if (input > 0 && input < 100) {
+     gridConversion(input);
+}
