20c149e78b5a8caf19283335e0d0114bc576e3e9
Sid <sidbanks@gmail.com>
2022-03-30 17:50:34 -0500

    Added Variable to determine size of grid squares. Added grid square size styling to grid square creation

--- script.js
+++ script.js
@@ -7,10 +7,15 @@ const grid = document.querySelector('.grid');
 //Declaration of lengthSquare variable
 let lengthSquare = sideLength * sideLength;
 
+//Declaration of variable for grid square size
+let setSide = `${640 / sideLength}px`;
+
 //Function for creating grid squares using sideLength as a parameter
 function createSquares() {
     for (let i = 0; i < lengthSquare; i++) {
         let gridSquare = document.createElement('div');
+        gridSquare.style.width = setSide;
+        gridSquare.style.height = setSide;
         gridSquare.classList.add('gridSquare');
         grid.appendChild(gridSquare);
     }
