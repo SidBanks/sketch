44b26a4ab020f851edb4491dd95a49425bc7851d
Sid <sidbanks@gmail.com>
2022-03-30 04:28:46 -0500

    Corrected function for creating Grid Divs.  Added Variable for Square side length

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
-// Variables for grid size and input.
-let gridSize = 255;
-let input;
+//Declaration of variable for the length of the sides and its default value
+let sideLength = 16;
 
-// function that takes input from gridSize button and squares it then
-// sets it to the gridBox variable.
-function gridConversion(input) {
-     gridSize = input * input;
-     return gridSize;
+//Creation of grid DOM item.
+const grid = document.querySelector('.grid');
+
+//Declaration of lengthSquare variable
+let lengthSquare = sideLength * sideLength;
+
+//Function for creating grid squares using sideLength as a parameter
+function createSquares() {
+    for (let i = 0; i < lengthSquare; i++) {
+        let gridSquare = document.createElement('div');
+        gridSquare.classList.add('gridSquare');
+        grid.appendChild(gridSquare);
+    }
 }
 
-// function using for loop and gridBox variable to create divs
-function gridLayout(gridSize) {
-     for (let i = 0; i < gridSize; i++) {
-          let g[i] = document.createElement('div');
-     }
-}
-
-// function for using button to resize grid.
-input = prompt("Please enter a number between 0 -100 to resize the sketch pad.");
-if (isNaN(input)) {
-     alert('Please Enter a number');
-} else if (input > 0 && input < 100) {
-     gridConversion(input);
-}
+createSquares(lengthSquare);
