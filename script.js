936de5a6ef9c4ad225df9c2af070545de88e5d09
Sid <sidbanks@gmail.com>
2022-04-19 01:41:17 -0500

    Added grid clearing if loop to eleminate creating endless grids on button click.  New issue is that sizes have te be selected twice before pixels will be drawn.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let pixelSize = document.querySelector('#pixelSize');
 
-//Declaration of side length variable, and length square variable
+//Declaration of side length variable, length square, pixel size,
+// and pen color variable.
 let sideLength;
 let pixelTotal;
 let setSide;
@@ -34,18 +35,28 @@ grid.style.flexWrap = 'wrap';
 grid.style.borderStyle = 'solid';
 grid.style.borderColor = 'black';
 grid.style.backgroundColor = 'transparent';
-grid.style.height = '640px';
 grid.style.width = '640px';
+grid.style.height = '640px'
+grid.style.flexGrow= 1;
 
 //Function for creating grid squares using sideLength as a parameter
 function createPixels(pixelTotal) {
+    if (grid.hasChildNodes()) {
+        while (grid.hasChildNodes()) {
+            grid.removeChild(grid.firstChild);
+        }
+    } else {
     for (let i = 0; i < pixelTotal; i++) {
         let pixelSquare = document.createElement('div');
+        pixelSquare.display = 'flex';
         pixelSquare.style.width = setSide;
         pixelSquare.style.height = setSide;
+        pixelSquare.style.flexShrink = 2;
+        pixelSquare.style.minWidth = '6.4px';
         pixelSquare.classList.add('pixelSquare');
         grid.appendChild(pixelSquare);
     }
+}
     let pixels = document.querySelectorAll('.pixelSquare');
 
 //Event listener loop for grid squares
@@ -59,9 +70,9 @@ function createPixels(pixelTotal) {
 }
 
 //event listeners for pixel size and drawing
-pixelSize.addEventListener('click', function() { setSideLength(pixelSize); });
-pixelSize.addEventListener('click', function() { pixelMaker(sideLength); });
-pixelSize.addEventListener('click', function() { pixelSizer(sideLength); });
-pixelSize.addEventListener('click', function() { createPixels(pixelTotal); });
+pixelSize.addEventListener('input', function() { setSideLength(pixelSize); });
+pixelSize.addEventListener('input', function() { pixelMaker(sideLength); });
+pixelSize.addEventListener('input', function() { pixelSizer(sideLength); });
+pixelSize.addEventListener('input', function() { createPixels(pixelTotal); });
 
 
